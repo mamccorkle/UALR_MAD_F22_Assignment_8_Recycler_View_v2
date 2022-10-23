7bb1119bb5251695783325cda4e7368384d6f3c4
Mark McCorkle <mamccorkle@ualr.edu>
2022-10-23 16:43:27 -0500

    Initial Commit - 202210231643
    Added project files
    Added document signing
    Added README.md
    Project template builds and runs

---
+++ README.md
@@ -0,0 +1,26 @@
+# UALR - Mobile Application Development - F22 - Assignment 6 - Android Recycler View
+
+## CPSC 3367 Assignment 6 Topics:
+
+<img title="" src="file:///Users/decyple/Downloads/RecyclerIcon.png" alt="" data-align="center">
+
+> Android Application Development - Using the Android Recycler View
+
+#### Instructions:
+
+> :heavy_check_mark::x:
+
+> :x: TODO 01. Generate the item list to be displayed using the DataGenerator class
+> :x: TODO 02. Add RecyclerView element here
+> :x: TODO 03. Do the setup of a new RecyclerView instance to display the item list properly
+> :x: TODO 04. Define the layout of each item in the list
+> :x: TODO 05. Create a new Adapter class and the corresponding ViewHolder class in a different file. The adapter will be used to populate the recyclerView and manage the interaction with the items in the list
+> :x: TODO 06. Detect click events on the list items. Implement a new method to toggle items' selection in response to click events
+> :x: TODO 07. Detect click events on the thumbnail located on the left of every list row when the corresponding item is selected. And, Implement a new method to delete the corresponding item in the list
+> :x: TODO 08. Create a new method to add a new item on the top of the list. Use the DataGenerator class to create the new item to be added.
+> :x: TODO 09. Create a new instance of the created Adapter class and bind it to the RecyclerView instance created in step 03
+> :x: TODO 10. Invoke the method created to a new item to the top of the list so it's triggered when the user taps the Floating Action Button
+
+#### Submission Due Date:
+
+>  Source Due: Friday, November 4, 2022 11:59 PM
---
+++ app/src/main/java/com/ualr/recyclerviewassignment/MainActivity.java
@@ -0,0 +1,62 @@
+// https://github.com/UALR-MobileAppsDevelopmentCourse/assignment-06-mamccorkle
+//
+//  UALR - MAD - F22 - Assignment 6 - Android Recycler View
+//
+//  Project: UALR - Mobile Applications Development - Fall 2022 - Assignment 6 - Android Recycler View
+//  Created by: Mark McCorkle on 20221020
+//  Based on: Code Provided by Dr Ivan Rodriguez-Conde
+//
+//  IDE:
+//     Android Studio Chipmunk | 2021.2.1 Patch 2
+//     Build #AI-212.5712.43.2112.8815526, built on July 10, 2022
+//     Runtime version: 11.0.12+0-b1504.28-7817840 x86_64
+//     VM: OpenJDK 64-Bit Server VM by JetBrains s.r.o.
+//     macOS 11.5.2
+//     GC: G1 Young Generation, G1 Old Generation
+//     Memory: 2048M
+//     Cores: 8
+//     Registry: external.system.auto.import.disabled=true
+//
+package com.ualr.recyclerviewassignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+// TODO 05. Create a new Adapter class and the corresponding ViewHolder class in a different file. The adapter will be used to populate
+//  the recyclerView and manage the interaction with the items in the list
+// TODO 06. Detect click events on the list items. Implement a new method to toggle items' selection in response to click events
+// TODO 07. Detect click events on the thumbnail located on the left of every list row when the corresponding item is selected.
+//  Implement a new method to delete the corresponding item in the list
+// TODO 08. Create a new method to add a new item on the top of the list. Use the DataGenerator class to create the new item to be added.
+
+public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton mFAB;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_multi_selection);
+        initComponent();
+    }
+
+    private void initComponent() {
+        // TODO 01. Generate the item list to be displayed using the DataGenerator class
+        // TODO 03. Do the setup of a new RecyclerView instance to display the item list properly
+        // TODO 04. Define the layout of each item in the list
+        // TODO 09. Create a new instance of the created Adapter class and bind it to the RecyclerView instance created in step 03
+        mFAB = findViewById(R.id.fab);
+        mFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // TODO 10. Invoke the method created to a new item to the top of the list so it's
+                //  triggered when the user taps the Floating Action Button
+            }
+        });
+    }
+
+}
